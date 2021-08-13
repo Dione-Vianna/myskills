9f20994865eb1cd5ec3a94a64328a37dfe278831
dione-vianna <dioneg12@hotmail.com>
2021-08-13 19:42:46 -0300

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View, Platform, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 export function Home() {
+  const [skills, setSkills] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+
+  function newSkills() {}
   return (
     <View style={styles.container}>
       <Text style={styles.title}>My skills</Text>
@@ -9,7 +20,20 @@ export function Home() {
         style={styles.input}
         placeholder="My skills"
         placeholderTextColor="#999"
+        onChangeText={setSkills}
       />
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.7}
+        onPress={setSkills}
+      >
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+      <Text style={styles.skillsText}>My skillsText</Text>
+
+      <TouchableOpacity style={styles.buttonSkill}>
+        <Text style={styles.textSkill}>Teste</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -36,4 +60,29 @@ const styles = StyleSheet.create({
     marginTop: 30,
     borderRadius: 7,
   },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+  skillsText: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  buttonSkill: {
+    color: '#fff',
+    backgroundColor: '#1f1E25',
+    padding: 20,
+  },
+  textSkill: {},
 });
